be803debc8742bc42f5408ceb0092b7415096590
MChandraR <2201020103@student.umrah.ac.id>
2024-06-04 14:55:36 +0700

    Project updates

--- map.py
+++ map.py
@@ -1,179 +1,218 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTk
+import tkinter as tk
+from tkinter import ttk
 import random
-import math
+from numpy import sort
 
-# Ukuran gambar dan jumlah langkah
 scale = 10
 width = 150 * scale
 height = 150 * scale
-max_count = 20
-roadWidth = 15
+padding = scale
+road_width = 2 *scale
+maps = Image.new("RGBA", (width , height), "gray")
+mapDraw = ImageDraw.Draw(maps,"RGBA")
+lastVertices = [(0,0)]
+count = 0
+roadx, roady = Image.open("assets/roads/roadss-x.png"), Image.open("assets/roads/roadss-y.png")
+cornerR, cornerL= Image.open("assets/roads/cornersr2.png") , Image.open("assets/roads/cornersl2.png")
+t= Image.open("assets/roads/t.png")
+tl= Image.open("assets/roads/tl.png")
+tr= Image.open("assets/roads/tr.png")
+building = [ 
+    [Image.open("assets/buildings/large-x.png"), Image.open("assets/buildings/large-y.png")],[Image.open("assets/buildings/large2-x.jpg"), Image.open("assets/buildings/large2-y.jpg")],
+    [Image.open("assets/buildings/medium-x.jpg"), Image.open("assets/buildings/medium-y.jpg")],[Image.open("assets/buildings/medium2-x.jpg"), Image.open("assets/buildings/medium2-y.jpg")],
+    [Image.open("assets/buildings/small-x.jpg"), Image.open("assets/buildings/small-y.jpg")],
+    # [Image.open("assets/buildings/small2-x.jpg"), Image.open("assets/buildings/small2-y.jpg")],
+    [Image.open("assets/buildings/house-x.png"), Image.open("assets/buildings/house-y.png")]
+]
 
-streetv = Image.open("image/streetv.png")
-streeth = Image.open("image/streeth.png")
-building = Image.open("image/buildings.png")
-school = Image.open("image/school.png")
-house = Image.open("image/house.png")
-buildings = [building,house,school]
-tree = Image.open("image/tree.png")
-pinus = Image.open("image/pinuss.png")
-image = Image.new("RGBA", (width, height), color="green")
-images = Image.new("RGBA", (width, height), color="green")
-draw = ImageDraw.Draw(image)
-draws = ImageDraw.Draw(images)
-trees = [tree,pinus]
-#vertexList = [(0,0),(width-(2*scale),height-(2*scale)),(0,height-(2*scale)),(width-(2*scale),0)]
-vertexList = []
+decoration = [ Image.open("assets/decor/tree1.png"),Image.open("assets/decor/plan1.png"),Image.open("assets/decor/plan2.png"),Image.open("assets/decor/plant3.png"),Image.open("assets/decor/plant4.png"),Image.open("assets/decor/stone.png"),Image.open("assets/decor/genangan.png")]
 
-def generateVertex(width, height, previousVertex, vertexList):
-    min_distance = min(width, height) / 15  #Jarak minimum antar vertex
-    while True:
-        edge = random.choice(['x', 'y'])
+def drawArea(pos1, pos2,text):
+    if abs(pos1[0] - pos2[0]) > road_width and abs(pos1[1] - pos2[1]) > road_width:
+        ysort = sort((pos1[1], pos2[1])) + (padding if pos1[1] > road_width else -padding, -padding)
+        xsort = sort((pos1[0], pos2[0])) + (padding if pos1[0] > road_width else -padding, -padding)
+        mapDraw.rectangle(((xsort[0], ysort[0]),(xsort[1], ysort[1])), "green")
+        posY = ysort[0]
+        tertinggi = 10
+        while posY < ysort[1] :
+            posX = xsort[0] 
+            posY = posY if posY <= ysort[1] else ysort[1]
+            if posY < 0 :continue
+            while posX < xsort[1] - padding:
+                if (posY == ysort[0] ) or (posY == ysort[1] ) :
+                    builds = [build for build in building if posX + build[0].size[0]< xsort[1]]
+                    if len(builds):
+                        img = random.choice(builds)
+                        tertinggi = max(img[0].size[1] +5, tertinggi)
+                        maps.paste(img[0], (posX, posY if posY != ysort[1] else ysort[1]-img[0].size[1]))
+                        posX += img[0].size[0] + 5
+                    else :break
+                else: 
+                    if posX >= xsort[1] - 50: posX = xsort[1] - 50
+                    if posY + 50 >= ysort[1] -50 and ysort[1] - posY >= 50:
+                        posY = ysort[1]
+                        continue 
+                    builds = [build[1] for build in building if posX + build[1].size[0]< xsort[1] and posY+build[1].size[1] < ysort[1] - 50]
+                    decor = [decor for decor in decoration if decor.size[0] + posX <= xsort[1] and decor.size[1] + posY <= ysort[1] - 50]
+                    if len(builds):
+                        img = random.choice(builds if (posX == xsort[0]  or posX >= xsort[1]-55) else decor)
+                        tertinggi = max(img.size[1] +5, tertinggi ) 
+                        maps.paste(img, (posX if posX < xsort[1]-50 else xsort[1] - img.size[0], posY + random.randint(0,(tertinggi )-(img.size[1]))))
+                        posX += img.size[0] + 5 if posX < xsort[1]-55 else xsort[1] + 5
+                    else :break
+            posY += tertinggi
+ 
+def drawRoad(start,end,dir):
+    for ver in range(start[0] if dir=="x" else start[1], end[0] if dir=="x" else end[1], road_width): maps.paste(roadx if dir == "x" else roady , (ver if dir=="x" else start[0],start[1] if dir=="x" else ver))
 
-        if edge == 'x':
-            x = previousVertex[0]
-            y = random.randint(0, height)
-        else:
-            x = random.randint(0, width)
-            y = previousVertex[1]
+def drawCorner(pos, dir):maps.paste(cornerL if dir=="l" else cornerR, pos)
 
-        if all(math.sqrt((x - v[0])**2 + (y - v[1])**2) >= min_distance for v in vertexList):
-            valid_point = True
-            for i in range(len(vertexList) - 1):
-                x1, y1, _ = vertexList[i]
-                x2, y2, _ = vertexList[i + 1]
-                distance = abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1) / math.sqrt((y2 - y1)**2 + (x2 - x1)**2)
-                if distance < min_distance:
-                    valid_point = False
-                    break
+def mapping(ver1,ver2,ver3,ver4,adver,hah,text):
+    global count
+    if (ver1[1] == ver2[1] and ver1[0] == ver4[0]) and (ver2[0] == ver3[0] and ver4[1] == ver3[1]) and len(hah) < 5 : drawArea((ver1[0]+road_width,ver1[1]+road_width), (ver3[0], ver3[1]), text)
+    elif (ver1[1] == ver2[1] and ver1[0] == ver4[0]) and (ver2[0] == ver3[0] and ver4[1] == ver3[1]) and len(hah) >= 5  :
+        max_atas = max([data[1] for data in hah])
+        drawArea((ver1[0]+road_width,max_atas+road_width), (ver3[0], ver3[1]), text)
+      #Bentuk die l kebalik
+    elif ver1[1] >  ver2[1]  and text == "normal" :
+        max_atas = max([data[1] for data in hah])
+        minx = min([ver[0] for ver in hah if ver[0] > ver4[0] and ver[1] < max_atas])
+        mapping((ver4[0], ver1[1] if len(hah) <= 3 else max_atas), (ver2[0], ver1[1] if len(hah) <= 3 else max_atas), ver3, ver4, [],[], "bawah")
+        mapping((minx,adver[-1:][0][1]), ( ver2[0], adver[-1:][0][1]), (ver2[0], ver1[1] + ((road_width+padding) if ver1[1] < ver3[1] else 0)), (adver[-2:-1][0][0] if len(adver) > 1 else adver[-1:][0][0], ver1[1] + ((road_width+padding) if ver1[1] < ver3[1] else 0)), [], [],"atas")
+    # #bentuk die L
+    elif ver1[1] < ver2[1] and text == "normal":
+        max_atas = max([data[1] for data in hah])
+        mapping((ver1[0], ver2[1]), (ver2[0], ver2[1]), ver3, ver4, adver, [], "bawah")
+        mapping(ver1,hah[-2:-1][0] if len(hah) > 2 else hah[-1:][0], (hah[-2:-1][0][0] if len(hah) >2 else hah[-1:][0][0],ver2[1] + ((road_width+padding) if ver2[1] < ver3[1] else 0)),(ver1[0], ver2[1]+ ((road_width+padding) if ver2[1] < ver3[1] else 0)),[],[],"atas")
+    count +=1
 
-            if valid_point:
-                return x, y, edge
+def limitX(x): return x if x < width else width
+def limitY(y): return y if y < height else height
 
-def firstVertex(width, height):
-    edge = random.choice(['atas', 'bawah', 'kiri', 'kanan'])
+#Algo Jalan dan mapping area
+def makePoint(pos, save_point):
+    nextJump = (limitX(pos[0]+ random.randint(2,4) * 10*scale),  limitY(random.randint(2,4) * 10*scale))
+    limit_atass, limit_atasss = pos[1], pos[1]
+    midver = []
+    max_atas = 0
+    overlap = False
+    hah = [(pos)]
+    prev = lastVertices[0]
+    for ver in lastVertices:
+        if pos[0] > prev[0] and pos[0] < ver[0]: 
+            limit_atass = ver[1] 
+            if prev != ver : prev = ver
+        if ver[0] < nextJump[0]:
+            if ver[0]>=pos[0] : 
+                if not len(midver): midver = [ver]
+                else :midver.append(ver)
+            limit_atasss = ver[1]
+            max_atas = ver[1] if ver[1] > max_atas else max_atas
+        if ver[0] < nextJump[0] and ver[0] > pos[0]: hah.append(ver)
+    maks = max(pos[1],max_atas,limit_atasss+nextJump[1])
+    nextJump = (nextJump[0], maks if maks<= height else height )
+    for ver in save_point: 
+        if ver[0] == pos[0] and ver[1] > nextJump[1]: overlap = True
+    corcount = sum( [ver == (nextJump[0], limit_atasss) for ver in lastVertices])
+    drawCorner((pos[0], nextJump[1]-road_width), "l") if (pos[0], nextJump[1]) not in save_point and not overlap and pos[0]> 0 else drawRoad((pos[0], nextJump[1]),(pos[0]+road_width, nextJump[1]),"x")
+    if(nextJump[0]+road_width<=width and nextJump[1]  < height):drawCorner((nextJump[0]-road_width, nextJump[1]-road_width), "r")
+    if(pos[0]>0): drawRoad((pos[0] , limit_atass+ (road_width if limit_atass>0 else 0) ),(pos[0], nextJump[1]), "y")
+    drawRoad((pos[0]+road_width, nextJump[1]),(nextJump), "x")
+    drawRoad((nextJump[0], limit_atasss+(road_width if not corcount else 0)),(nextJump), "y")
+    save_point.extend(((pos[0], nextJump[1]) , nextJump))
+    mapping((pos[0],limit_atass), (nextJump[0], limit_atasss), nextJump, (pos[0], nextJump[1]), midver,hah, "normal")
+    if(pos[0] >= 0 and pos[0] < width): 
+        makePoint((nextJump[0], limit_atasss), save_point)
+    else:       
+        if(pos[1] >= 0 and pos[1]+200 < height):
+            lastVertices.clear()  
+            lastVertices.extend((save_point))
+            makePoint((0, lastVertices[0][1]), [])
     
-    if edge == 'atas':
-        x = random.randint(0, width)
-        y = 0
-    elif edge == 'bawah':
-        x = random.randint(0, width)
-        y = height
-    elif edge == 'kiri':
-        x = 0
-        y = random.randint(0, height)
-    else:
-        x = width
-        y = random.randint(0, height)
+vportx , vporty = 0,0
+zoom_factor = 1.0
+INITIAL_WIDTH = 500
+INITIAL_HEIGHT = 400
+viewport_x = 0
+viewport_y = 0
+viewport_width = 400
+viewport_height = 400
+
+def generateMap():
+    global maps,mapDraw, lastVertices, count, out
+    maps = Image.new("RGBA", (width , height), "gray")
+    mapDraw = ImageDraw.Draw(maps,"RGBA")
+    lastVertices = [(0,0)]
+    count = 0
+    makePoint((0,0), [])
+    maps.save("map1.png")
+    new = Image.new("RGBA", (width , height), "green")
+    out = Image.alpha_composite( new,maps)
+    out.save("map2.png")
+    cropped_map = out.crop((vportx, vporty, viewport_x + viewport_width, viewport_y + viewport_height))
+    resized_map = cropped_map.resize((INITIAL_WIDTH, INITIAL_HEIGHT))
+    img_tk = ImageTk.PhotoImage(resized_map)
+    map_label.config(image=img_tk)
+    map_label.image = img_tk
+    update()
+    
+
+    
+    
+def update():
+    print("zoom")
+    global out
+    cropped_map = out.crop((viewport_x * zoom_factor, viewport_y * zoom_factor, viewport_x* zoom_factor + viewport_width* zoom_factor, viewport_y* zoom_factor + viewport_height* zoom_factor))
+    resized_map = cropped_map.resize((INITIAL_WIDTH, INITIAL_HEIGHT))
+    img_tk = ImageTk.PhotoImage(resized_map)
+    map_label.config(image=img_tk)
+    map_label.image = img_tk
+    
+def zoom_in():
+    global zoom_factor
+    if zoom_factor < 5.0:
+        zoom_factor += 0.1
+        update()
+
+# Fungsi untuk zoom out
+def zoom_out():
+    global zoom_factor
+    if zoom_factor > 0.5:
+        zoom_factor -= 0.1
+        update()
         
-    return x, y
-
-def lastVertex(width, height, previousVertex):
-    edge = previousVertex[2]
-    if(edge == 'x'):
-        edge = 'y'
+def scroll(event):
+    global viewport_x, viewport_y
+    if event.delta > 0:
+        # Scroll ke atas
+        viewport_y -= 20
     else:
-        edge = 'x'
+        # Scroll ke bawah
+        viewport_y += 20
+    update()
+            
 
-    if edge == 'x':
-        x = previousVertex[0]
-        if(previousVertex[1] > height/2):
-            y = height
-        else:
-            y = 0
-    else:
-        if(previousVertex[0] > width/2):
-            x = width
-        else:
-            x = 0
-        y = previousVertex[1]
+root = tk.Tk()
+root.title("Map Generator")
+root.bind("<MouseWheel>", scroll)
 
-    return x, y, edge
+frame = ttk.Frame(root, padding=10)
+frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
-def drawArea(x,y,x1,y1,side):
-    padding  = 2*scale
-    x += padding ; y+= padding
-    if x >= x1-scale or y >= y1-scale:
-        return
-    curX,curY = x ,y
-    
-    gedung = random.choice(buildings)
-    if  gedung.size[0] < (x1-x-scale) and  gedung.size[1] < (y1-y-scale):
-        image.paste(gedung,(x,y))
-    elif (x1-x) > tree.size[0] and (y1-y) > tree.size[1] :
-        gedung = random.choice(trees)
-        image.paste(gedung,(x,y))
-    while (curX + gedung.size[0] + padding) < x1 and side:
-        size = gedung.size[0] + scale
-        if y+building.size[0]-padding < y1:
-            drawArea(curX+size,y-padding,x1,y+building.size[0]-padding,False)
-        curX += size + scale
-    while (curY + gedung.size[1] + padding) < y1 and side:
-        size = gedung.size[1] + scale
-        if x+building.size[1]-padding < x1:
-            drawArea(x-padding,curY+size,x+building.size[1]-padding,y1,False)
-        curY += size + scale
-    
-    if (x+gedung.size[0]-padding+scale)<x1 and (y+gedung.size[1]-padding+scale) < y1 and not side:
-        drawArea(x+gedung.size[0],y+gedung.size[1],x1,y1,True)
+map_label = ttk.Label(frame)
+map_label.grid(row=0, column=0, padx=10, pady=10)
 
-def search():
-    for idx, ver in enumerate(vertexList):
-        minX  = 0
-        nearX = width
-        nearY = height
-        minY = 0
-        maxX = 0
-        maxY = 0
-        for i in range(0,len(vertexList)):
-            if i == idx :
-               continue
-            if vertexList[i][0] > ver[0] and vertexList[i][0]  < nearX:
-                nearX = vertexList[i][0]
-            if vertexList[i][1] > ver[1] and vertexList[i][1]  < nearY:
-                nearY = vertexList[i][1]
-            if vertexList[i][0] >= minX and vertexList[i][0] < ver[0]:
-               minX = vertexList[i][0]
-               maxY = vertexList[i][1]
-            if vertexList[i][1] >= minY and vertexList[i][1] < ver[1]:
-               minY = vertexList[i][1]
-               maxX = vertexList[i][0]
-        if minX > 0 and minY > 0:
-            print("jumpa")
-            if (minX,minY) not in vertexList:
-                vertexList.append((minX,minY)) 
-            if (maxX,maxY) not in vertexList:
-                vertexList.append((maxX,maxY))
-            print(ver, minX,minY)
-            drawArea(minX+scale,minY+scale,ver[0],ver[1],True)
-        if (nearX,nearY) not in vertexList:
-            vertexList.append((nearX,nearY))
-        if minX == 0 or minY == 0:
-            drawArea(minX,minY,ver[0],ver[1],True)
-       
-#generate firstVertex
-previousVertex = firstVertex(width, height)
-vertexList.append(previousVertex + ('',))
+generate_button = ttk.Button(root, text="Generate Map", command=generateMap)
+generate_button.grid(row=1, column=0, pady=10)
 
-#generate random Vertex
-for _ in range(max_count - 2):
-    nextVertex = generateVertex(width, height, previousVertex, vertexList)
-    draw.line((previousVertex[0], previousVertex[1], nextVertex[0], nextVertex[1]), fill='black', width=roadWidth)
-    #draw.ellipse((nextVertex[0] - 2, nextVertex[1] - 2, nextVertex[0] + 2, nextVertex[1] + 2), fill='red')
-    previousVertex = nextVertex
-    vertexList.append(nextVertex)
+zoom_in_button = ttk.Button(root, text="Zoom In", command=zoom_in)
+zoom_in_button.grid(row=2, column=0, pady=5)
 
-#generate lastVertex
-endVertex = lastVertex(width, height, previousVertex)
-vertexList.append(endVertex)
-draw.line((previousVertex[0], previousVertex[1], endVertex[0], endVertex[1]), fill='black', width=roadWidth)
+generateMap()
+zoom_out_button = ttk.Button(root, text="Zoom Out", command=zoom_out)
+zoom_out_button.grid(row=3, column=0, pady=5)
 
-search()
-print(vertexList)
-print(len(vertexList))
-for ver in vertexList:
-   draws.rectangle(xy = (ver[0],ver[1],ver[0]+(2*scale),ver[1]+(2*scale)),fill=(0,0,0))
-# Menyimpan gambar sebagai file
-image.show()
-#image.save("map2.png")
-#images.save("vertices.png")
+root.mainloop()
+
+# out.show()
